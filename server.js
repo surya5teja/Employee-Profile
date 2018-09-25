d9508b54eed1843e551e69c646e0d7bd88ddd6ce
surteja <surteja@deloitte.com>
2018-09-25 20:10:05 +0530

    express changes1

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/src/employee-profile'));
 
-//app.get('/*', function(req, res) {
-  //  res.sendFile(path.join(__dirname + '/src/index.html'));
-  //});
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/src/employee-profile/index.html'));
+  });
 
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8083);
+app.listen(process.env.PORT || 8080);
