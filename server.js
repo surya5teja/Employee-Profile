9bc2075d8aafd712fadf0e7612c433d76be9b85f
surteja <surteja@deloitte.com>
2018-09-27 17:42:56 +0530

    package json changes3

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const path = require('path');
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+app.get('/*', function(req,res) {
+    
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    });
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
